69d34240dfe4102e71c0a6300a22f5079410ef4a
averyBennett <abennett1162@gmail.com>
2024-10-03 18:27:51 -0500

    Update 1.2 Noise.cs
    Got rid of some of the library "using" calls at the top of the program

--- Noise.cs
+++ Noise.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Numerics;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.Xml;
-using System.Windows.Forms;
 
 public class Noise
 {
